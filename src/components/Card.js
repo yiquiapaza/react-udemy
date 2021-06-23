b9ae5089910cd494450bcaf6d282752d359f774c
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-06-22 23:35:10 -0300

    Fist time I use useState hook

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 const Card = (props) => {
-	return <div>{props.children}</div>;
+  return <div>{props.children}</div>;
 };
 
 export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,22 @@
-import ExpenseDate from "./ExpenseDate";
+import { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+//React render all components the first time then, you can't modify the DOM with simple function
+//TODO: Create markdonw file, and write notes with the most important  concept that learned in this section
 const ExpenseItem = (props) => {
-    return (
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle('Updated!')
+  };
+  return (
     <div>
-        <div>
-            <ExpenseDate date = {props.date}/>
-            <h2>{props.title}</h2>
-            <div>{props.amount}</div>
-        </div>
+      <div>
+        <ExpenseDate date={props.date} />
+        <h2>{title}</h2>
+        <div>{props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+      </div>
     </div>
-    )
+  );
 };
 
 export default ExpenseItem;
