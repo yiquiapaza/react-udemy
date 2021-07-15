1ed3f6740979eb4cc64f0d3e9551e12911619bcd
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-07-15 16:10:12 -0300

    Add ExpresesFilter

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,17 +3,12 @@ import ExpenseDate from './ExpenseDate';
 //React render all components the first time then, you can't modify the DOM with simple function
 //TODO: Create markdonw file, and write notes with the most important  concept that learned in this section
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle('Updated!')
-  };
   return (
     <div>
       <div>
         <ExpenseDate date={props.date} />
-        <h2>{title}</h2>
-        <div>{props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
+        <h2>{props.title}</h2>
+        <div>${props.amount}</div>
       </div>
     </div>
   );
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,32 @@
+import { useState } from 'react';
+
 import Card from './Card';
 import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from './ExpensesFilter';
 const Expenses = (props) => {
   console.log(props);
 
+  const [filteredYear, setFilterYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
   let extensesToReader = props.expenses.map((item) => {
     return (
-      <Card key={item.id}>
-        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
-      </Card>
+      <ExpenseItem
+        key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      />
     );
   });
-  return <div>{extensesToReader}</div>;
+  return (
+    <Card>
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <div>{extensesToReader}</div>;
+    </Card>
+  );
 };
 export default Expenses;
