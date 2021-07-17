5fd4b6608ca1eb75ae08663db6de4debe6c0940a
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-07-16 23:48:33 -0300

    Update card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 //React render all components the first time then, you can't modify the DOM with simple function
 //TODO: Create markdonw file, and write notes with the most important  concept that learned in this section
